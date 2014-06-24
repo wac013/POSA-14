8bf913ed9f53046225fb25908debafc5f1c841ba
Christopher Wollett <christopher.wollett@gmail.com>
2014-06-23 23:55:39 -0400

    assignment-4

--- assignments/week-5-assignment-4/W5-A4-Android/src/edu/vuum/mocca/AndroidPlatformStrategy.java
+++ assignments/week-5-assignment-4/W5-A4-Android/src/edu/vuum/mocca/AndroidPlatformStrategy.java
@@ -47,6 +47,7 @@ public class AndroidPlatformStrategy extends PlatformStrategy
     {
         /** (Re)initialize the CountDownLatch. */
         // TODO - You fill in here.
+    	mLatch = new CountDownLatch(NUMBER_OF_THREADS);
     }
 
     /** Print the outputString to the display. */
@@ -57,18 +58,50 @@ public class AndroidPlatformStrategy extends PlatformStrategy
          * and appends the outputString to a TextView. 
          */
         // TODO - You fill in here.
+    	final Activity activity = mActivity.get();
+    	if(activity != null)
+    	{
+    		activity.runOnUiThread(new Runnable() {
+				@Override
+				public void run() {
+					if(mTextViewOutput != null)
+						mTextViewOutput.append(outputString + '\n');
+				}
+			});
+    	}
+    	else	// no activity? we're done
+    		done();
     }
 
     /** Indicate that a game thread has finished running. */
     public void done()
     {	
         // TODO - You fill in here.
+    	final Activity activity = mActivity.get();
+    	if(activity != null)
+    	{
+    		activity.runOnUiThread(new Runnable() {
+				@Override
+				public void run() {
+					mLatch.countDown();
+				}
+			});
+    	}
+    	else
+    		mLatch.countDown();
     }
 
     /** Barrier that waits for all the game threads to finish. */
     public void awaitDone()
     {
         // TODO - You fill in here.
+        try
+        {
+            mLatch.await();
+        }
+        catch(java.lang.InterruptedException e)
+        {
+        }
     }
 
     /** 
